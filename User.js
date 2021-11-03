a104260b7fbab55a78e80d47f6b7455f5bdac44b
Kumol Yotech <kumol@yotech.ltd>
2021-11-03 17:31:24 +0600

    lk

--- User.js
+++ User.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 module.exports = mongoose.model("User",mongoose.Schema({
 	name: String,
 	id: {
-            type: Number,
-            autoIncrement: true,
-            primaryKey: true,
-        },
+        type: Number,
+        autoIncrement: true,
+        primaryKey: true,
+    },
 	password: String
 }));
